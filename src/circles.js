919382da9dd92ae05fc2bcc10367bb64d26b6980
= <=>
2022-12-27 03:55:45 +0100

    changed the background effect to an infinite falling snow effect

--- src/circles.js
+++ src/circles.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const navbar = document.getElementsByTagName("nav")[0];
-const colors = ['#9400D3', '#4B0082', '#0000FF', '#00FF00','#FFFF00', '#FF7F00', '#FF0000'];
 window.requestAnimationFrame = window.requestAnimationFrame ||
                                window.mozRequestAnimationFrame ||
                                window.webkitRequestAnimationFrame ||             
@@ -13,11 +12,6 @@ function init() {
     window.innerHeight - navbar.getBoundingClientRect().height
   );
 
-  const particles = generateParticles();
-  particles.forEach(particle => {
-    particle.draw();
-  });
-
   /*
     calcute the remaining viewport by subtracting the height of the window the height of the navbar
     */
@@ -25,6 +19,7 @@ function init() {
 
 class Particle {
   constructor() {
+    Math.floor()
     this.x = Math.floor(Math.random() * canvas.width);
     this.y = Math.floor(Math.random() * canvas.height);
     this.size = Math.floor(Math.random() * 6);
@@ -32,7 +27,9 @@ class Particle {
   }
 
   draw() {
+    console.log(this.x.toString());
     ctx.beginPath();
+    console.log()
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fillStyle = this.color;
     ctx.fill(),
@@ -42,7 +39,7 @@ class Particle {
 const generateParticles = () => {
   const particles = [];
 
-  for (let i = 0; i < 150; i++) {
+  for (let i = 0; i < 200; i++) {
     const particle = new Particle;
     particles.push(particle);
   }
@@ -50,19 +47,37 @@ const generateParticles = () => {
   return particles;
 }
 
-const clear = () => {
-  ctx.clearRect(0,0,canvas.width, canvas.height)
-}
+init();
 
-const update = () => {
-  clear();
-  const particles = generateParticles();
+const particles = generateParticles();
+
+const initializeEffect = () => {
   particles.forEach(particle => {
     particle.draw();
   });
 }
 
-init();
-window.onscroll = () => {
-update()
+const clear = () => {
+  ctx.clearRect(0,0,canvas.width, canvas.height)
 }
+
+const update = () => {
+
+  particles.forEach(particle => {
+    particle.y = particle.y += Math.random() * 4;
+    if(particle.y > canvas.height) {
+      particle.y = 0;
+    }
+    particle.draw();
+  });
+}
+
+
+console.log(canvas.width)
+
+initializeEffect();
+
+setInterval(() => {
+  clear();
+  update();
+}, 1000 / 30)
